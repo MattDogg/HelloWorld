c62f883dbc2825c20b59abb068a052b9bdc89994
Matt <mdickerson@solstice-mobile.com>
2014-10-29 09:07:30 -0500

    added randomGreetings function to randomly choose elements from array

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-    let greetings = ["Hi","Hello","Howdy","Hey"]
+    let greetings: [String] = ["Hi","Hello","Howdy","Hey"]
     var cycle = 0
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var buttonLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let aGreeting = randomGreetings()
+        println(aGreeting)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    /*
+    aGreeting() -> String {
+    
+        
+    }
+    returns aGreeting
+    
+    
+    func sample()  {
+        
+    }
+    returns nothing
+    */
+
+
+    func randomGreetings() -> String {
+        
+        let randomIndex = arc4random_uniform(UInt32(greetings.count))
+        let randomGreetings = greetings[randomIndex.hashValue]
+        
+        return randomGreetings
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +53,7 @@ class ViewController: UIViewController {
     @IBAction func pushButton(nameButton: UIButton) {
        // buttonLabel.text = "hi(nameTextField.text)"
     }
+    
     @IBAction func helloWorldAction(nameTextField: UITextField) {
         nameLabel.text = "\(greetings[cycle]) \(nameTextField.text)"
         cycle = cycle + 1
