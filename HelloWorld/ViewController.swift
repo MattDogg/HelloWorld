5e74e8b564769ee0895bed4d86fba37a4eb89bd6
Matt <mdickerson@solstice-mobile.com>
2014-10-30 09:05:19 -0500

    implemented randomGreetings function, added comments and value/type exercise

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,33 +9,51 @@
 import UIKit
 import Foundation
 
+class Sample {
+
+    
+}
+
 class ViewController: UIViewController {
-    let greetings: [String] = ["Hi","Hello","Howdy","Hey"]
-    var cycle = 0
+    
+    
+    let listOne = [2, 3, 5, 6]
+    let valueOne = 2
+    
+    let listTwo = ["one", "two", "three"]
+    let valueTwo = "one"
+    
+    let listThree = [Sample(), Sample(), Sample()]
+    
+    
+    //What is the type of listOne?
+    //What is the type of valueOne?
+    //What is the type of listOne[2]?
+    //What is the value of listOne[2]?
+    //What is the value of valueOne?
+    
+    //What is the type of listTwo?
+    //What is the type of valueTwo?
+    //What is the type of listTwo[2]?
+    //What is the value of listTwo[2]?
+    //What is the value of valueTwo?
+    
+    
+    //What is the type of listThree?
+    //What is the type of listThree[2]?
+    
+    
+    
+    let greetings:[String] = ["Hi","Hello","Howdy","Hey"]
+    var cycle:Int = 0
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var buttonLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let aGreeting = randomGreetings()
-        println(aGreeting)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    /*
-    aGreeting() -> String {
     
-        
-    }
-    returns aGreeting
-    
-    
-    func sample()  {
-        
-    }
-    returns nothing
-    */
-
-
     func randomGreetings() -> String {
         
         let randomIndex = arc4random_uniform(UInt32(greetings.count))
@@ -55,11 +73,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func helloWorldAction(nameTextField: UITextField) {
-        nameLabel.text = "\(greetings[cycle]) \(nameTextField.text)"
+       
+        //e.g. let myGreeting = "hi"
+        let myGreeting: String = randomGreetings()
+        //alternate way --> let myGreeting: String = greetings[cylce]
+        
+        //What appears in text field
+        nameLabel.text = "\(myGreeting) \(nameTextField.text)"
+       
+       
+        /*
+            Example of cycling through elements of array
         cycle = cycle + 1
         if cycle > countElements(greetings)-1 {
             cycle = 0
-        }
+        }*/
     }
 }
 
